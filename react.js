7eda378305aea15fec6e4237dd7d226e27502c8a
shadow-eljan <eljanrai25@gmail.com>
2025-05-26 07:22:15 +0545

    feat: API mock test

--- react.js
+++ react.js
@@ -1,3 +1,33 @@
-const arr1=[1,2,3,4,5]
-const arr2=[...arr1]
-console.log(arr2)
+
+const arr=[1, 2, 3, 4, 5]
+const filtereven =()=>{
+  const result= arr.filter((num)=> num % 2==0)
+    return result
+}
+console.log(filtereven())
+
+const UserData=[
+  {id:1, name:"eljan",gender:"male",age:19,occupation:"web dev"},
+  {id:2, name:"subash",gender:"male",age:17,occupation:"web dev"},
+  {id:3, name:"sumit",gender:"male",age:19,occupation:"designer"},
+  {id:4, name:"gyanisha",gender:"female",age:35,occupation:undefined},
+  {id:5, name:"nishum",gender:"female",age:18,occupation:undefined},
+]
+const response={
+  status:200,
+  message:"UserData phased successfully",
+  data:UserData
+}
+
+const FetchUserNames=()=>{
+  if(response.status === 200){
+   const result = response.data
+   .filter((user)=>user.occupation !== undefined || user.age > 30)
+   .map((user)=>user.name)
+   return result
+  }else{
+    const message="APT Fetch failes!!"
+    return message
+  }
+}
+console.log(FetchUserNames())
